0f64e30a76285bafcf846df634a9ca79a5c8af74
Brian Isaac Quindi <brianquindi1@gmail.com>
2025-06-12 22:03:47 -0300

    Initial commit

---
+++ TpFinalPNT-PilApp/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from './components/navbar.vue'
+import Footer from './components/footer.vue'
+</script>
+
+<template>
+  
+  <Navbar />
+    <router-view />
+  <Footer/>
+
+</template>
+
+<style scoped>
+
+</style>
